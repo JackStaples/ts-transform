d0f317ef3e08ff8c9dc941a182589d23bc9e8618
Jack Staples <JackStaplesjcrs@gmail.com>
2021-03-21 21:35:02 -0700

    Add Transformer class and test stub

---
+++ Transformer.ts
@@ -0,0 +1,20 @@
+import Converter from "./types/Converter.ts";
+
+class Transformer<I, O> {
+  private _converter;
+
+  constructor(converter: Converter<I, O>) {
+    this._converter = converter;
+  }
+
+  convertOne(input: I): O {
+    const retVal = {} as O;
+    const keys = Object.keys(this._converter) as Array<keyof Converter<I, O>>;
+    for (const key of keys) {
+      const func = this._converter[key];
+      const val = func(input);
+      retVal[key] = val;
+    }
+    return retVal;
+  }
+}

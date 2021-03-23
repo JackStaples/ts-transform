b0050f62beb3ea5d0cb1e1ec7b08ca9f026c4db8
Jack Staples <JackStaplesjcrs@gmail.com>
2021-03-23 15:02:33 -0700

    Add multi type conversion test

--- Tests/Transformer.test.ts
+++ Tests/Transformer.test.ts
@@ -4,16 +4,28 @@ import { assertEquals } from "https://deno.land/std@0.90.0/testing/asserts.ts";
 
 type stringType = { a: string; b: string; c: string };
 type numberType = { a: number; b: number; c: number };
+type mixedType = { a: string; b: numberType; c: boolean };
 
-const stringToNumber: Converter<stringType, numberType> = {
-  a: (input: stringType) => Number.parseInt(input.a),
-  b: (input: stringType) => Number.parseInt(input.b),
-  c: (input: stringType) => Number.parseInt(input.c),
-};
-
-Deno.test("It can convert strings to numbers", () => {
+Deno.test("It can convert between a string object and a number object", () => {
+  const stringToNumber: Converter<stringType, numberType> = {
+    a: (input: stringType) => Number.parseInt(input.a),
+    b: (input: stringType) => Number.parseInt(input.b),
+    c: (input: stringType) => Number.parseInt(input.c),
+  };
   const input = { a: "22", b: "33", c: "44" };
   const transformer = new Transformer(stringToNumber);
   const output = transformer.convertOne(input);
   assertEquals(output, { a: 22, b: 33, c: 44 });
+});
+
+Deno.test("It can convert to multiple different types", () => {
+  const numberToMixed: Converter<numberType, mixedType> = {
+    a: (input) => String(input.a),
+    b: (input) => input,
+    c: (input) => input.c === 3,
+  };
+  const input = { a: 22, b: 33, c: 44 };
+  const transformer = new Transformer(numberToMixed);
+  const output = transformer.convertOne(input);
+  assertEquals(output, { a: String(input.a), b: input, c: input.c === 3 });
 });

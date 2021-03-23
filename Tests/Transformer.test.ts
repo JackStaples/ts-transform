e3057272141fe5add27d15a06b29a0792e9fe91b
Jack Staples <JackStaplesjcrs@gmail.com>
2021-03-23 13:50:42 -0700

    Add simple string to number conversion

--- Tests/Transformer.test.ts
+++ Tests/Transformer.test.ts
@@ -1,6 +1,19 @@
 import Transformer from "../Transformer.ts";
-import { assert } from "https://deno.land/std@0.90.0/testing/asserts.ts";
+import Converter from "../types/Converter.ts";
+import { assertEquals } from "https://deno.land/std@0.90.0/testing/asserts.ts";
 
-Deno.test("It can do a simple transformation", () => {
-  assert(true);
+type stringType = { a: string; b: string; c: string };
+type numberType = { a: number; b: number; c: number };
+
+const stringToNumber: Converter<stringType, numberType> = {
+  a: (input: stringType) => Number.parseInt(input.a),
+  b: (input: stringType) => Number.parseInt(input.b),
+  c: (input: stringType) => Number.parseInt(input.c),
+};
+
+Deno.test("It can convert strings to numbers", () => {
+  const input = { a: "22", b: "33", c: "44" };
+  const transformer = new Transformer(stringToNumber);
+  const output = transformer.convertOne(input);
+  assertEquals(output, { a: 22, b: 33, c: 44 });
 });

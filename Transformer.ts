de4800551b461c04555f3e90eedcad7be28dbc5a
Jack Staples <JackStaplesjcrs@gmail.com>
2021-03-24 20:10:52 -0700

    move key pulling to constructor

--- Transformer.ts
+++ Transformer.ts
@@ -5,15 +5,16 @@ export default class Transformer<
   O extends Record<PropertyKey, unknown>,
 > {
   private _converter;
+  private _keys: Array<keyof Converter<I, O>>;
 
   constructor(converter: Converter<I, O>) {
     this._converter = converter;
+    this._keys = Object.keys(this._converter);
   }
 
   convertOne(input: I): O {
     const retVal = {} as O;
-    const keys = Object.keys(this._converter) as Array<keyof Converter<I, O>>;
-    for (const key of keys) {
+    for (const key of this._keys) {
       const func = this._converter[key];
       const val = func(input);
       retVal[key] = val;
@@ -23,10 +24,9 @@ export default class Transformer<
 
   convertMany(inputs: Array<I>): Array<O> {
     const retVal = [] as Array<O>;
-    const keys = Object.keys(this._converter) as Array<keyof Converter<I, O>>;
     for (let i = 0; i < inputs.length; i++) {
       retVal[i] = {} as O;
-      for (const key of keys) {
+      for (const key of this._keys) {
         const func = this._converter[key];
         const val = func(inputs[i]);
         retVal[i][key] = val;
